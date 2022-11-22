22b8eb5086d1d5959058ad3219970006fa35e0cb
KennethArn <96439063+KennethArn@users.noreply.github.com>
2022-11-22 18:59:28 +0100

    Remote ef attrbutes from "view" model

--- CNRBShopAPI/Models/Product.cs
+++ CNRBShopAPI/Models/Product.cs
@@ -5,19 +5,12 @@ namespace CNRBShopAPI.Models
 {
     public class Product 
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ProductId { get; set; }
         public string ProductName { get; set; } = string.Empty;
         public decimal Price { get; set; }    
         public bool IsProductOfTheWeek { get; set; }
         public bool InStock { get; set; }
-
-        /* Do i need to specify this verification in the models */
-        //[ForeignKey("CategoryId")]
         public int CategoryId { get; set; }
-
-        public Category? Category { get; set; } /*= default!; */
-
+        public Category? Category { get; set; }
     }
 }
